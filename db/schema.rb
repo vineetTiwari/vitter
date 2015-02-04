7fc7ffc8eab2a6b09314c8bfb7cef129bdd3a5f5
Vinny Tiwari <tiwari.vineet@outlook.com>
2015-02-03 22:43:58 -0800

    added replies model and fixed db

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150204033334) do
+ActiveRecord::Schema.define(version: 20150204064102) do
 
   create_table "replies", force: :cascade do |t|
     t.text     "body"
     t.integer  "tweet_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "replies", ["tweet_id"], name: "index_replies_on_tweet_id"
+
+  create_table "tweets", force: :cascade do |t|
+    t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
